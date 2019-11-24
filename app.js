a409c4dc08957cbe713a07bf58ee0f6025e29183
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-11-24 16:36:18 -0500

    Add SQLite database access and test multipart formdata
    Also added vscode launch task and few other setup files

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const sqlite3 = require('sqlite3').verbose();
+const multer = require('multer');
+const upload = multer();
+
+let db = new sqlite3.Database('./db/elections.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the elections database.');
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/create', function (req, res) {
+  res.json({ code: Math.round(Math.random() * 10) });
+});
+
+app.get('/vote', upload.none(), function (req, res) {
+  
+  const formData = req.body;
+  
+  let jsonData = JSON.parse(formData.data);
+  
+  res.json({ code: formData.code, data: jsonData });
+  
 });
 
 app.listen(3000, function () {
