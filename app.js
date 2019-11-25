a3b3690df61309198e80c459aab33705a4a630db
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-11-24 19:28:12 -0500

    Convert to tab indentation

--- app.js
+++ app.js
@@ -5,30 +5,30 @@ const multer = require('multer');
 const upload = multer();
 
 let db = new sqlite3.Database('./db/elections.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the elections database.');
+	if (err) {
+		console.error(err.message);
+	}
+	console.log('Connected to the elections database.');
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.get('/create', function (req, res) {
-  res.json({ code: Math.round(Math.random() * 10) });
+	res.json({ code: Math.round(Math.random() * 10) });
 });
 
 app.get('/vote', upload.none(), function (req, res) {
-  
-  const formData = req.body;
-  
-  let jsonData = JSON.parse(formData.data);
-  
-  res.json({ code: formData.code, data: jsonData });
-  
+	
+	const formData = req.body;
+	
+	let jsonData = JSON.parse(formData.data);
+	
+	res.json({ code: formData.code, data: jsonData });
+	
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
