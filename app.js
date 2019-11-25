df37c464150b86da1b8dc93c4a00c1d640e02f31
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-11-24 20:39:31 -0500

    Tweak app.js format to adhere to my own conventions

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import multer from 'multer';
 const sqlite3 = require('sqlite3').verbose();
-const multer = require('multer');
+
+const app = express();
 const upload = multer();
 
-let db = new sqlite3.Database('./db/elections.db', (err) => {
+let db = new sqlite3.Database("./db/elections.db", err => {
 	if (err) {
 		console.error(err.message);
 	}
 	else {
 		// Connected to database successfully
-		console.log('Connected to the elections database.');
+		console.log("Connected to the elections database.");
 	}
 });
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+app.get("/", (req, res) => {
+	res.send("Hello World!");
 });
 
-app.get('/create', function (req, res) {
+app.get("/create", (req, res) => {
 	res.json({ code: Math.round(Math.random() * 10) });
 });
 
-app.get('/vote', upload.none(), function (req, res) {
+app.get("/vote", upload.none(), (req, res) => {
 	
 	const formData = req.body;
 	
@@ -32,6 +33,6 @@ app.get('/vote', upload.none(), function (req, res) {
 	
 });
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+app.listen(3000, () => {
+	console.log("Scouts Elections Server app listening on port 3000!");
 });
