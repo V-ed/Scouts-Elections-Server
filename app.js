a08283b646ef04f8cb80603d2ad8fa687601de80
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-11-26 01:36:46 -0500

    Comment out unused db variable temporarrly

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const multer = require('multer');
-const sqlite3 = require('sqlite3').verbose();
+// const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const upload = multer();
 
 const PORT = 3000;
 
-let db = new sqlite3.Database("./db/elections.db", err => {
-	if (err) {
-		console.error(err.message);
-	}
-	else {
-		// Connected to database successfully
-		console.log("Connected to the elections database.");
-	}
-});
+// let db = new sqlite3.Database("./db/elections.db", err => {
+// 	if (err) {
+// 		console.error(err.message);
+// 	}
+// 	else {
+// 		// Connected to database successfully
+// 		console.log("Connected to the elections database.");
+// 	}
+// });
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
