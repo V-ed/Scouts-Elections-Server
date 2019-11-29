afeca1089c120ab6f272509b1de98c3719f46758
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-11-28 22:02:33 -0500

    Tweak app.js to be used as a module
    This makes up for the change made in my own server (to run manually managed modules on a single DNS with custom webroot).

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const express = require('express');
 const multer = require('multer');
 // const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
-const https = require('https');
 
-const app = express();
 const upload = multer();
 
-const PORT = 3001;
-
-// let db = new sqlite3.Database("./db/elections.db", err => {
+// let db = new sqlite3.Database(`${__dirname}/db/elections.db`, err => {
 // 	if (err) {
 // 		console.error(err.message);
 // 	}
@@ -19,38 +13,24 @@ const PORT = 3001;
 // 	}
 // });
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
-
-app.get("/create", (req, res) => {
-	res.json({ code: Math.round(Math.random() * 10) });
-});
-
-app.get("/vote", upload.none(), (req, res) => {
+exports.setup = (app, webroot) => {
 	
-	const formData = req.body;
+	app.get(`${webroot}`, (req, res) => {
+		res.send("Hello World!");
+	});
 	
-	let jsonData = JSON.parse(formData.data);
+	app.get(`${webroot}/create`, (req, res) => {
+		res.json({ code: Math.round(Math.random() * 10) });
+	});
 	
-	res.json({ code: formData.code, data: jsonData });
+	app.get(`${webroot}/vote`, upload.none(), (req, res) => {
+		
+		const formData = req.body;
+		
+		let jsonData = JSON.parse(formData.data);
+		
+		res.json({ code: formData.code, data: jsonData });
+		
+	});
 	
-});
-
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/ved.ddnsfree.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/ved.ddnsfree.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/ved.ddnsfree.com/chain.pem', 'utf8');
-
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
-
-// Starting server
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(PORT, () => {
-	console.log(`HTTPS : Scouts Elections Server app listening on port ${PORT}!`);
-});
+}
