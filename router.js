70e7d04a26cf67e041a779347d2e2ff6a0d14027
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-12-07 20:02:54 -0500

    Add CORS autorizations for this API's request

--- router.js
+++ router.js
@@ -1,20 +1,21 @@
 const multer = require('multer');
 const { ElectionController } = require('./app');
+const cors = require('cors')
 
 const upload = multer();
 
 exports.setup = (app, route) => {
 	
-	app.post(route('/create'), upload.none(), ElectionController.create);
+	app.post(route('/create'), upload.none(), cors(), ElectionController.create);
 	
-	app.get(route('/:electionCode([A-Z1-9]{6})/join'), ElectionController.join);
+	app.get(route('/:electionCode([A-Z1-9]{6})/join'), cors(), ElectionController.join);
 	
-	app.put(route('/:electionCode([A-Z1-9]{6})/vote'), upload.none(), ElectionController.vote);
+	app.put(route('/:electionCode([A-Z1-9]{6})/vote'), upload.none(), cors(), ElectionController.vote);
 	
-	app.put(route('/:electionCode([A-Z1-9]{6})/skip'), ElectionController.skip);
+	app.put(route('/:electionCode([A-Z1-9]{6})/skip'), cors(), ElectionController.skip);
 	
-	app.get(route('/:electionCode([A-Z1-9]{6})/retrieve'), ElectionController.retrieve);
+	app.get(route('/:electionCode([A-Z1-9]{6})/retrieve'), cors(), ElectionController.retrieve);
 	
-	app.delete(route('/:electionCode([A-Z1-9]{6})/delete'), ElectionController.delete);
+	app.delete(route('/:electionCode([A-Z1-9]{6})/delete'), cors(), ElectionController.delete);
 	
 }
