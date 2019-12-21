d4213a52b39ddbc4ab57d9ce99bd5a1e6f889d0c
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-12-21 14:56:01 -0500

    Update routes to match new API scheme
    Closes #14

--- router.js
+++ router.js
@@ -4,20 +4,20 @@ const express = require('express');
 
 exports.setup = (app, route) => {
 	
-	app.post(route('/create'), express.json({ limit: '3mb' }), cors(), ElectionController.create);
-	
 	app.options(route('/*'), cors());
 	
-	app.get(route('/:electionCode([A-Z1-9]{6})/join'), cors(), ElectionController.join);
+	app.post(route('/create'), express.json({ limit: '3mb' }), cors(), ElectionController.create);
 	
-	app.put(route('/:electionCode([A-Z1-9]{6})/vote'), express.json({ limit: '10kb' }), cors(), ElectionController.vote);
+	app.get(route('/join/:electionCode([A-Z1-9]{6})'), cors(), ElectionController.join);
 	
-	app.get(route('/:electionCode([A-Z1-9]{6})/seat'), cors(), ElectionController.takeSeat);
+	app.put(route('/vote/:electionCode([A-Z1-9]{6})'), express.json({ limit: '10kb' }), cors(), ElectionController.vote);
 	
-	app.put(route('/:electionCode([A-Z1-9]{6})/skip'), cors(), ElectionController.skip);
+	app.get(route('/seat/:electionCode([A-Z1-9]{6})'), cors(), ElectionController.takeSeat);
 	
-	app.get(route('/:electionCode([A-Z1-9]{6})/retrieve'), cors(), ElectionController.retrieve);
+	app.put(route('/skip/:electionCode([A-Z1-9]{6})'), cors(), ElectionController.skip);
 	
-	app.delete(route('/:electionCode([A-Z1-9]{6})/delete'), cors(), ElectionController.delete);
+	app.get(route('/retrieve/:electionCode([A-Z1-9]{6})'), cors(), ElectionController.retrieve);
+	
+	app.delete(route('/delete/:electionCode([A-Z1-9]{6})'), cors(), ElectionController.delete);
 	
 }
