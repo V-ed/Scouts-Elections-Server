d4c4c47231a6c2b9122b63b27a92c67ed5360b08
jfs39 <jfs.serg@hotmail.com>
2020-01-05 18:39:26 -0500

    In this commit, I replaced the Regex in router.js in order that they exclude the letter O to reduce ambiguity in codes.

--- router.js
+++ router.js
@@ -12,16 +12,16 @@ exports.setup = (app, route) => {
 	
 	app.post(route('/create'), express.json({ limit: '3mb' }), cors(), sync(), ElectionController.create);
 	
-	app.get(route('/join/:electionCode([A-Z1-9]{6})'), cors(), sync(), ElectionController.join);
+	app.get(route('/join/:electionCode([A-NP-Z1-9]{6})'), cors(), sync(), ElectionController.join);
 	
-	app.put(route('/vote/:electionCode([A-Z1-9]{6})'), express.json({ limit: '10kb' }), cors(), sync(), ElectionController.vote);
+	app.put(route('/vote/:electionCode([A-NP-Z1-9]{6})'), express.json({ limit: '10kb' }), cors(), sync(), ElectionController.vote);
 	
-	app.get(route('/seat/:electionCode([A-Z1-9]{6})'), cors(), sync(), ElectionController.takeSeat);
+	app.get(route('/seat/:electionCode([A-NP-Z1-9]{6})'), cors(), sync(), ElectionController.takeSeat);
 	
-	app.put(route('/skip/:electionCode([A-Z1-9]{6})'), cors(), sync(), ElectionController.skip);
+	app.put(route('/skip/:electionCode([A-NP-Z1-9]{6})'), cors(), sync(), ElectionController.skip);
 	
-	app.get(route('/retrieve/:electionCode([A-Z1-9]{6})'), cors(), sync(), ElectionController.retrieve);
+	app.get(route('/retrieve/:electionCode([A-NP-Z1-9]{6})'), cors(), sync(), ElectionController.retrieve);
 	
-	app.delete(route('/delete/:electionCode([A-Z1-9]{6})'), cors(), sync(), ElectionController.delete);
+	app.delete(route('/delete/:electionCode([A-NP-Z1-9]{6})'), cors(), sync(), ElectionController.delete);
 	
 }
